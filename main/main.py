d7a6c5608f36f7be228abaf76a880bef863b3dee
laasso <nrubialesguerrero@gmail.com>
2024-04-17 14:14:56 +0200

    .bak error

--- main/main.py
+++ main/main.py
@@ -5,10 +5,9 @@ def read_urls_and_filenames(filename):
     urls_and_filenames = []
     with open(filename, 'r') as file:
         for line in file:
-            url, csv_filename = line.strip().split(',')
+            url, csv_filename = line.strip().split(', ')
             urls_and_filenames.append((url.strip(), csv_filename.strip()))
     return urls_and_filenames
-
 def main():
     scraper = WebScraper()
     urls_and_filenames = read_urls_and_filenames('../INFO/sites&files.txt')
